ee323a1eb05c8b9a9c0fbba71541e63feced34db
cbv520 <cblakevillegas@gmail.com>
2017-02-28 07:16:46 +0800

    aggrefation

--- National.java
+++ National.java
@@ -1,23 +1,37 @@
 public class National extends Organisation
 {
     private String sport;
+    private State[] states;
     
-    National(String inSport, String inName, String inContactName, String inContactEmail)
+    National
+    {
+        super("default","default","default");
+        sport = "default";
+        states = null;
+    }
+    
+    National(String inSport, String inName, String inContactName, String inContactEmail, State[] inStates)
     {
         super(inName, inContactName, inContactEmail);
         
         if(inSport == null || inSport.equals(""))
         {
             throw new IllegalArgumentException("Invalid sport");
-        }    
+        }
+        if(inStates == null)
+        {
+            throw new IllegalArgumentException("invalid states");
+        }
         
         sport = inSport;
+        states = inStates;
     }
     
     National(National otherNational)
     {
         super(otherNational);
         sport = otherNational.getSport();
+        states = otherNational.getStates();
     }
     
     public String getSport()
@@ -35,7 +49,7 @@ public class National extends Organisation
         sport = inSport;
     }
     
-    public void read()
+    public void read(String file, int row)
     {
         
     }
